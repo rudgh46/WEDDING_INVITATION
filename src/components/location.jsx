283e990e04c05890f2e45496d2876b3730e744cf
rudgh46 <rudgh46@naver.com>
2024-11-15 21:10:21 +0900

    수정3

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -103,23 +103,23 @@ const Location = () => {
         className="root_daum_roughmap root_daum_roughmap_landing"
       ></Map>
       <Content>
-        대구 수성구 두산동 888-2번지
+        서울 마포구 월드컵로 240
         <br />
-        호텔수성 수성스퀘어 3층 피오니홀
+        2층 임페리얼홀
         <br />
         <br />
         <Title>버스 이용시</Title>
         <br />
         <br />
-        410-1, 401 호텔수성 앞 하차
+        아직 안찾아봄
         <br />
-        수성1-1, 수성3-1, 814 TBC방송국 앞 하차
+        아직 안찾아봄
         <br />
         <br />
         <Title>지하철 이용시</Title>
         <br />
         <br />
-        3호선 수성못역 하차 (도보 10분)
+        6호선 월드컵경기장역 하차 (도보 5분)
       </Content>
     </Wrapper>
   );
