bb41892de04fd78dbb6d884ac7e5db5f757c56d7
rudgh46 <rudgh46@naver.com>
2024-11-15 21:56:40 +0900

    수정6

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-export const WEDDING_INVITATION_URL = "http://localhost:8000/";
-export const KAKAOTALK_API_TOKEN = "d030f90ee0e598eeb3902c23eaa362ce";
+export const WEDDING_INVITATION_URL = "https://kkh-khs.netlify.app";
+export const KAKAOTALK_API_TOKEN = "161cb0af5d02a3b8c8b158c0d65f5885";
 export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
-export const WEDDING_DATE = "2025년 11월 02일, 일요일 오후 13시 50분";
+export const WEDDING_DATE = "2025년 11월 2일 일요일 오후 1시 50분";
 export const WEDDING_LOCATION = "월드컵컨벤션, 2층 임페리얼홀";
 
 export const GROOM_NAME = "김경호";
@@ -15,7 +15,7 @@ export const GROOM_MOTHER_ACCOUNT_NUMBER = "신한은행 110-394-974712";
 
 export const BRIDE_NAME = "권효선";
 export const BRIDE_ACCOUNT_NUMBER = "신한은행 110-394-974712";
-export const BRIDE_FATHER_NAME = "○○○";
+export const BRIDE_FATHER_NAME = "권정근";
 export const BRIDE_FATHER_ACCOUNT_NUMBER = "신한은행 110-394-974712";
-export const BRIDE_MOTHER_NAME = "○○○";
+export const BRIDE_MOTHER_NAME = "임윤구";
 export const BRIDE_MOTHER_ACCOUNT_NUMBER = "신한은행 110-394-974712";
