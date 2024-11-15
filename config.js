eff25f1897372b7f5b833272d838000d54a29893
rudgh46 <rudgh46@naver.com>
2024-11-15 20:47:19 +0900

    [TEST01]-초기수정

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 export const WEDDING_INVITATION_URL = "http://localhost:8000/";
-export const KAKAOTALK_API_TOKEN = "JavaScript 키 입력";
+export const KAKAOTALK_API_TOKEN = "d030f90ee0e598eeb3902c23eaa362ce";
 export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
-export const WEDDING_DATE = "1970년 01월 01일, 목요일 오전 12시 00분";
-export const WEDDING_LOCATION = "○○○웨딩, ○층 ○○홀";
+export const WEDDING_DATE = "2025년 11월 02일, 일요일 오후 13시 50분";
+export const WEDDING_LOCATION = "월드컵컨벤션, 2층 임페리얼홀";
 
-export const GROOM_NAME = "○○○";
-export const GROOM_ACCOUNT_NUMBER = "○○은행 ***-***-******";
-export const GROOM_FATHER_NAME = "○○○";
-export const GROOM_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
-export const GROOM_MOTHER_NAME = "○○○";
-export const GROOM_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const GROOM_NAME = "김경호";
+export const GROOM_ACCOUNT_NUMBER = "신한은행 110-394-974712";
+export const GROOM_FATHER_NAME = "김세황";
+export const GROOM_FATHER_ACCOUNT_NUMBER = "신한은행 110-394-974712";
+export const GROOM_MOTHER_NAME = "차광석";
+export const GROOM_MOTHER_ACCOUNT_NUMBER = "신한은행 110-394-974712";
 
-export const BRIDE_NAME = "○○○";
-export const BRIDE_ACCOUNT_NUMBER = "○○은행 ***-***-******";
-export const BRIDE_FATHER_NAME = "○○○";
-export const BRIDE_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const BRIDE_NAME = "김경호";
+export const BRIDE_ACCOUNT_NUMBER = "신한은행 110-394-974712";
+export const BRIDE_FATHER_NAME = "ooo";
+export const BRIDE_FATHER_ACCOUNT_NUMBER = "신한은행 110-394-974712";
 export const BRIDE_MOTHER_NAME = "○○○";
-export const BRIDE_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const BRIDE_MOTHER_ACCOUNT_NUMBER = "신한은행 110-394-974712";
--- src/components/location.jsx
+++ src/components/location.jsx
@@ -47,8 +47,8 @@ const Location = () => {
   const executeScript = () => {
     const scriptTag = document.createElement("script");
     const inlineScript = document.createTextNode(`new daum.roughmap.Lander({
-    "timestamp" : "1652464367301",
-    "key" : "2a8fe",
+    "timestamp" : "1731671162872",
+    "key" : "2m97h",
     "mapWidth" : "640",
     "mapHeight" : "360"
   }).render();`);
